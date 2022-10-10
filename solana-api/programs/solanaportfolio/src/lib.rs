ccc827f630a541109ad241a5db459f7721547ee3
Mark <mark@silverstag.io>
2022-10-10 14:55:12 +0200

    Point to devnet

--- solana-api/programs/solanaportfolio/src/lib.rs
+++ solana-api/programs/solanaportfolio/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::entrypoint::ProgramResult;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("6VrvAT7HaxLE2iww8tViFkkCFGsc2pUVYrwDt6rE5asc");
 
 #[program]
 pub mod solanaportfolio {
